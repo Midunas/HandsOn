d048850630887817806e5c15961706a3835ecc96
Midunas <mykolas.vidunas@codeacademylt.onmicrosoft.com>
2022-09-29 10:13:54 +0300

    test(105):main page path change

--- src/app.tsx
+++ src/app.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => (
   <BrowserRouter>
     <Routes>
 
-      <Route path="/" element={<GlobalLayout />}>
+      <Route path="/HandsOn" element={<GlobalLayout />}>
         <Route index element={<ShopPage />} />
         <Route path="*" element={<NotFoundPage />} />
         <Route path="gifts" element={<GiftPage />} />
