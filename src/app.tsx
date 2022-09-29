f53ee578b9a47a8e95efd94e3b553e371bd24bcb
Midunas <mykolas.vidunas@codeacademylt.onmicrosoft.com>
2022-09-29 10:23:07 +0300

    test(105):Routes

--- src/app.tsx
+++ src/app.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => (
     <Routes>
 
       <Route path="/" element={<GlobalLayout />}>
-        <Route path="/HandsOn" element={<ShopPage />} />
+        <Route index element={<ShopPage />} />
         <Route path="*" element={<NotFoundPage />} />
         <Route path="gifts" element={<GiftPage />} />
         <Route path="clothing" element={<ClothingPage />} />
